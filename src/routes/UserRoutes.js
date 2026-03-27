d6f349b8aa2c041ae5431dc2f22c4334c17a6456
Shloka03 <srdave03@gmail.com>
2026-03-27 21:39:59 +0530

    forgotpassword

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,6 +4,8 @@ const { getProfile} = require("../controllers/UserController");
 const validateToken = require("../middleware/AuthMiddleware");
 router.post("/register",userController.registerUser)
 router.post("/login",userController.loginUser)
-router.get("/profile",validateToken,getProfile)
+router.get("/profile",validateToken,userController.getProfile)
+router.post("/forgotpassword",userController.forgotPassword)
+router.put("/resetpassword",userController.resetPassword)
 
 module.exports = router
